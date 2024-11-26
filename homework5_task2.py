2489be1463139e3bdeb65d3db24e3a93116864bf
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2024-11-26 10:28:04 +0300

    add

--- homework5_task2.py
+++ homework5_task2.py
@@ -14,35 +14,13 @@
 # На выходе:
 # {'Alice': 500.0, 'Bob': 300.0, 'Charlie': 1050.0}
 
-def calculate
-bonus(names, salary, bonus):
-_
-"""
-Рассчитывает бонус для каждого сотрудника.
-:param names: Список имен сотрудников.
-:param salary: Список зарплат сотрудников.
-:param bonus: Список премий в процентах.
-:return: Словарь с именем сотрудника в качестве ключа и суммой
-премии в качестве значения.
-"""
-# Генератор словаря для расчета бонуса: имя сотрудника -> сумма
-премии
-result = {names[i]: round(salary[i] * float(bonus[i].strip('%'))
-/ 100, 2) for i in range(len(names))}
-return result
-# Пример использования функции
-names = ["Alice"
-,
-"Bob"
-,
-"Charlie"]
+def calculate_bonus(names, salary, bonus):
+
+    result = {names[i]: round(salary[i] * float(bonus[i].strip('%')) / 100, 2) for i in range(len(names))}
+    return result
+
+names = ["Alice", "Bob", "Charlie"]
 salary = [5000, 6000, 7000]
-bonus = ["10%"
-,
-"5%"
-,
-"15%"]
-result = calculate
-bonus(names, salary, bonus)
-_
+bonus = ["10%", "5%", "15%"]
+result = calculate_bonus(names, salary, bonus)
 print(result)
